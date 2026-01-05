d7e1a444b873f44ad6c954c154690d37e8e4d484
banidoXD <68153273+banidoXD@users.noreply.github.com>
2026-01-05 19:32:42 -0300

    Update Hooks.js

--- scripts/Hooks.js
+++ scripts/Hooks.js
@@ -5,11 +5,19 @@ import {socketsHandle} from "./socketsHandler.js";
 Hooks.on("init", async () => await registerSettings());
 
 Hooks.on("ready", () => {
-    //event trigger for updating the data un the user side
+    // Escuta eventos de socket para atualizar dados
+    // Nota: Removi os parenteses de socketsHandle() se ele for a função de callback, 
+    // mas mantive como no original caso ele retorne uma função.
     game.socket.on('module.BonusDie', socketsHandle());
 })
 
-/* Magic one liner, I dont want to talk about this
-   bmarian made me do it, make issues on token tooltip alt
-   https://github.com/bmarian/token-tooltip-alt/issues */
-Hooks.on("renderPlayerList", (playerList, $playerList, players) => $playerList.find('ol').children().each(handle(players)));
+/* CORREÇÃO PARA FOUNDRY V13 */
+Hooks.on("renderPlayerList", (playerList, html, players) => {
+    // Na V13, 'html' é um elemento nativo (HTMLElement).
+    // O código original usa .find(), que é do jQuery.
+    // Solução: Envolvemos o html em $() para transformá-lo em jQuery de novo.
+    const $html = $(html);
+    
+    // Agora o .find funciona e passa os elementos jQuery para a função handle
+    $html.find('ol').children().each(handle(players));
+});
